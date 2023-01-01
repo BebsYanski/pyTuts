0c3cc509248b9798d25e63a1a3fd06ca3669c521
BebsYanski <bebongnchuy@gmail.com>
2023-01-01 10:12:44 +0100

    more

---
+++ Tutorials/first.py
@@ -0,0 +1,16 @@
+import math
+
+a = 16
+print(format(a, 'x'))  # it gives the number in hexadecimal(X)-b,o
+
+print("Hello world")
+print(f'Hello {a}')
+
+height = 1
+print(math.floor(2.2))
+print(2.3*2.3)
+print(2.3*13)
+
+name = "DevTown"
+print(name[0:3])
+slice(2,4)
